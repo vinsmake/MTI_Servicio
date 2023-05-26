15e9a8298e4fce7d477fcef239699e255fe355de
Enrique Plascencia <47471150+vinsmake@users.noreply.github.com>
2023-05-26 10:43:34 -0600

    Add files via upload

--- CV/js/script.js
+++ CV/js/script.js
@@ -1,20 +1,34 @@
 
 const button = document.querySelector("#data__button");
 let printArea = document.querySelector("#printArea");
-let cardArea = document.querySelector("#cardArea");
+let cvArea = document.querySelector("#cvArea");
+
 
 let contactLiObject = "";
 let profilePicObject = "";
 let contactLilinkObject = "";
 let habilidadesLiObject = "";
+let lgacObject = "";
+let tituloObject = "";
+let dependenciaObject = "";
+let fechaObject = "";
+let informacionObject = "";
+let formacionObject = "";
 
 function print(){
 let profilePic = document.querySelector("#data__profilePic");
-//let abbreviation = document.querySelector("#data__abbreviation");
+//here we have de data DOM
 let contactLiText = document.querySelector("#data__contactLiText");
 let contactLilink = document.querySelector("#data__contactLilink");
 let habilidadesLi = document.querySelector("#data__habilidadesLi");
+//here we have the academic DOM
+let lgac = document.querySelector("#formacion__lgac");
+let titulo = document.querySelector("#formacion__titulo");
+let dependencia = document.querySelector("#formacion__dependencia");
+let fecha = document.querySelector("#formacion__fecha");
+let informacion = document.querySelector("#formacion__informacion");
 
+    //data section
 //this changes the profile pic value if is not null
 if (profilePic.value != "") {
     profilePicObject = `${profilePic.value}`
@@ -37,37 +51,63 @@ if (habilidadesLi.value != "") {
     <li>${habilidadesLi.value}</li>`
 }
 
+    //formacion section
+//lgac
+if (lgac.value != "") {
+    lgacObject = `${lgac.value}`
+}
 
+
+
+if (dependencia.value != "" || fecha.value !="" || informacion.value !="" && titulo.value !=""){
+    formacionObject += `
+    <p style="font-weight: bold; font-size: 1rem; font-family: 'Noticia Text',serif; margin-bottom: 0;">${titulo.value}</p>
+    <p style="margin: 0; font-family: 'Open Sans','lucida grande',tahoma,verdana,arial,sans-serif;">${dependencia.value}<span style="font-size: 0.8rem;">[${fecha.value}]</span></p>
+    <p>${informacion.value}
+    </p>
+    `
+}
     printArea.value = 
 
 `		
-<div style="width: 60%; height: auto; margin: auto;">
-<img src="${profilePicObject}" style="width: 100%; height: auto; margin: auto; border-radius: 8px;" />
-</div>
-<h4 style="font-size: 1.5rem;">Contacto</h4>
-<ul style="margin: auto;">
-    ${contactLiObject}
-    <li><a href="https://www.linkedin.com/in/phdit-jaot-27900715/">LinkedIn</a></li>
-    <li><a href="https://orcid.org/0000-0001-5649-5514">ORCID</a></li>
-    <li>IEEE Miembro: 80390636 </li>
-</ul>
+<div style="width: 90%; margin: 1rem auto; gap: 1rem; display: flex; flex-wrap: wrap;">
+    <div style="margin: auto; padding: 1rem; border-radius: 8px; background-color: rgba(24,52,92,0.1); flex: 1 0 320px; box-shadow: 0px 0px 4px 2px rgba(0,0,0,0.50);">
+        <div style="width: 60%; height: auto; margin: auto;">
+            <img src="${profilePicObject}" style="width: 100%; height: auto; margin: auto; border-radius: 8px;" />
+        </div>
+    
+        <h4 style="font-size: 1.5rem;">Contacto</h4>
+        <ul style="margin: auto;">
+            ${contactLiObject}
+        </ul>
 
-<h4 style="font-size: 1.5rem;">Habilidades</h4>
-<ul style="margin: auto;">
-    ${habilidadesLiObject}
-    <li>Infraestructure as a Service</li>
-    <li>Security Systems</li>
-    <li>Distributed Systems</li>
-    <li>AI applied to Networks and Telecommunications</li>
-</ul>
+        <h4 style="font-size: 1.5rem;">Habilidades</h4>
+        <ul style="margin: auto;">
+            ${habilidadesLiObject}
+        </ul>
+    </div>
+
+    <div style="width: 100%; height: auto; flex: 1 0 430px;">
+        <h3>${lgacObject}</h3>
+        <h4 style="text-indent: 1rem;">Formación Académica:</h4>
+        <div>
+        ${formacionObject}
+        </div>
+    </div>
+</div>
 `
-    cardArea.innerHTML = printArea.value;
+    cvArea.innerHTML = printArea.value;
 
 
     profilePic.value = "";
     contactLiText.value = "";
     contactLilink.value = "";
     habilidadesLi.value = "";
+    lgac.value = "";
+    titulo.value = "";
+    dependencia.value = "";
+    fecha.value = "";
+    informacion.value = "";
 
 }
 
